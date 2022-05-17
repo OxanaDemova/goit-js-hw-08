cbc4a2e1a8026f6cd60cddd3f11a38477b07c9de
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-05-17 22:48:04 +0300

    +1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems.map((item) => {
+
+    const galleryImg = 
+    `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+    
+    return galleryImg
+})
+
+gallery.insertAdjacentHTML('beforeend', createGallery.join(''));
+
+let selectImg = new SimpleLightbox('.gallery a',
+    {
+        captionDelay: 250,
+        captionsData: 'alt'
+    }
+);
+
+
 
 console.log(galleryItems);
