cb4caa96f44182ff2f9c80ca2cc504ae9110071c
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-05-25 21:39:48 +0300

    2-3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import Player from '@vimeo/player';
+
+
+const throttle = require("lodash.throttle");
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const videoPlayer = document.querySelector("#vimeo-player");
+
+const player = new Player(videoPlayer);
+
+videoPlayer.addEventListener("load", onPlayerLoad);
+player.on("timeupdate", throttle(onPlayerRunning, 1000));
+
+function onPlayerRunning({ seconds }) {
+
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+function onPlayerLoad() {
+  try {
+    const lastVideoPosition = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    player
+      .setCurrentTime(lastVideoPosition)
+        .then(seconds => (seconds = lastVideoPosition))
+        
+      .catch(error => {
+        console.log(error.name);
+        console.log(error.message);
+      });
+
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+ 
+};
+
+const formData = {};
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+window.addEventListener('load', onPageLoad)
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  console.log(formDataObj);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+
+function onTextareaInput(e) {
+
+  const { name, value } = e.target;
+
+  formData[name] = value;
+ 
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+
+
+function onPageLoad() {
+  try {
+    const formDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    refs.input.value = formDataObj.email;
+    refs.textarea.value = formDataObj.message;
+
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
 
